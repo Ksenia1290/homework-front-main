eb8fd689da7d61d736b24d19f9949b97108620bb
Ksenia1290 <Ksu.tender@yandex.ru>
2024-01-06 20:40:49 +0300

    1

--- src/HW/1/UserList.tsx
+++ src/HW/1/UserList.tsx
@@ -1,28 +1,38 @@
 type AddressType = {
-  street: any; // ПОДПРАВЛЯЕМ any
-  city: any; // ПОДПРАВЛЯЕМ any
+  street: string; // ПОДПРАВЛЯЕМ any
+  city: string; // ПОДПРАВЛЯЕМ any
 };
 
 type UserType = {
-  // ПРИДЕТСЯ САМОМУ)
+ map(arg0: (el: UserType, index: number) => import("react/jsx-runtime").JSX.Element): unknown;
+ id: number;
+ name: string;
+ age: number;
+ address: Array<AddressType>; // ПРИДЕТСЯ САМОМУ)
 };
 
 type UserListPropsType = {
-  users: UserType; // ПО МОЕМУ ЧЕГО-ТО НЕ ХВАТАЕТ...
+  users: UserType [];
+  user:any;
+   // ПО МОЕМУ ЧЕГО-ТО НЕ ХВАТАЕТ...
 };
 
-export const UserList = (props: 'ВСТАВЛЯЕМ ТИПИЗАЦИЮ') => {
+export const UserList = (props: UserListPropsType) => {
+
+  const users = props.users.map((el:UserType, index: number) => {// ВСЕ ТОВАРЫ В СТРАНУ ПРИЕЗЖАЮТ В КОНТЕЙНЕРАХ, А В РЕАКТЕ...
+    return(
+     <li key={el.id} id={`hw01-user-${el.id}`}>
+     <strong>{el.name}</strong> (Age: {el.age})<strong> Address:</strong>
+     {props.user.address.street}, {props.user.address.city}
+    </li>
+    )
+    })
   return (
     <div id={'hw01-users'}>
       <h2>User List:</h2>
 
       <ul>
-          {props.users.map((user) => ( // ВСЕ ТОВАРЫ В СТРАНУ ПРИЕЗЖАЮТ В КОНТЕЙНЕРАХ, А В РЕАКТЕ...
-          <li key={ЭЛЕМЕНТ МАССИВА.id} id={`hw01-user-${ЭЛЕМЕНТ МАССИВА.id}`}>
-           <strong>{ЭЛЕМЕНТ МАССИВА.name}</strong> (Age: {ЭЛЕМЕНТ МАССИВА.age})<strong> Address:</strong>
-           {ЭЛЕМЕНТ МАССИВА.address.street}, {ЭЛЕМЕНТ МАССИВА.address.city}
-          </li>
-        ))}
+          {users}
       </ul>
     </div>
   );
