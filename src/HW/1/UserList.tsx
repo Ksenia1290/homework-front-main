8f498ab1025e80ab981888e8669cf7dd6f2a3943
Ksenia1290 <Ksu.tender@yandex.ru>
2024-01-29 00:39:21 +0300

    1

--- src/HW/1/UserList.tsx
+++ src/HW/1/UserList.tsx
@@ -1,4 +1,4 @@
-import { Key, ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from "react";
+
 
 type AddressType = {
   street: string, 
@@ -9,11 +9,11 @@ type UserType = {
   id: number,
   name: string,
   age: number, 
-  address:AddressType[]
+  address:AddressType
 };
 
 type UserListPropsType = {
-  users: any ,
+  users: UserType[] ,
 };
 
 export const UserList = (props: UserListPropsType) => {
@@ -22,12 +22,14 @@ export const UserList = (props: UserListPropsType) => {
       <h2>User List:</h2>
 
       <ul>
-        {props.users.map((user: { id: Key | null | undefined; name: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; age: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; address: { street: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; city: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; }; }) => ( 
-          <li key={user.id} id={`hw01-user-${user.id}`}>
-            <strong>{user.name}</strong> (Age: {user.age})<strong> Address:</strong>
+        {props.users.map((user) => (  
+        <li key={user.id} id={`hw01-user-${user.id}`}>
+            <strong>{user.name}</strong> 
+            (Age: {user.age})
+            <strong> Address:</strong>
             {user.address.street}, {user.address.city}
-          </li>
-        ))}
+          </li>)
+        )}
       </ul>
     </div>
   );
