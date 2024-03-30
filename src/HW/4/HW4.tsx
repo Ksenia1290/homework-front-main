543354050e1f6b471de42113cb0745ddd169cb6c
Ksenia1290 <Ksu.tender@yandex.ru>
2024-03-31 00:09:23 +0300

    1

--- src/HW/4/HW4.tsx
+++ src/HW/4/HW4.tsx
@@ -27,7 +27,7 @@ export const HW4 = () => {
         <h1 id={'hw04-default-text'}>Здесь появится новое дело</h1>
       )}
 
-<Input setCurrentText={setCurrentText}  currentText={currentText}/>
+<Input setCurrentText={setCurrentText}  currentText={currentText} onChange={handleChange}/>
 <Button  name={'+'} callBack={handleSave} />
 
 
